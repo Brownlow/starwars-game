b0003553b6d43a96256fb54806751b9fa54ed4d2
Ryan B <ryan@rbdesignd.com>
2018-01-19 17:14:15 -0800

    setup project

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+// User selects fighter to be their character
+
+// set user to this fighter
+
+// player chooses an opponent
+
+// These two charcters are separated from the unchosen characters
+
+// choosePlayer function(){}
+
+// chooseEnemy function(){}
+
+// display fight scene
+
+// user attack
+
+// opponent attack
+
+// defeat opponent
+
+// loose match
+
+// new opponent, etc
+
+
+
